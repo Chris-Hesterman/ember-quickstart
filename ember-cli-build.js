7aa9920ef03ecb6b7ee2f407173452a880fd9e39
Chris Hesterman <chrishesterman14@gmail.com>
2021-04-18 11:10:25 -0700

    Add vendor/netlify.toml file, modify ember-cli-build to add it to /dist dir

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,7 +6,9 @@ module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
   });
-
+  app.import('vendor/netlify.toml', {
+    destDir: '/',
+  });
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
